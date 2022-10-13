1395bd036e8369a82890de3c11db1a4f55edddfe
Liza1040 <maxsimova2002eliza@yandex.ru>
2022-10-13 13:00:46 +0300

    Добавлена реализация всплывающего окна

---
+++ PopUp.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const popUpBackground = document.getElementById('popUp-background');
+const openPopUp = document.getElementById('openPopUp');
+const popUp = document.getElementById('popUp');
+
+console.log(openPopUp);
+openPopUp.addEventListener('click', (event) => {
+    event.preventDefault();
+    popUp.classList.add('active');
+})
+
+document.addEventListener('click', (event) => {
+    if (event.target === popUpBackground) {
+        popUp.classList.remove('active');
+    }
+})
+
+openPopUp.onclick = (event) => {
+    event.preventDefault();
+    popUp.classList.add('active');
+}
+
+document.onclick = (event) => {
+    if (event.target === popUpBackground) {
+        popUp.classList.remove('active');
+    }
+}
